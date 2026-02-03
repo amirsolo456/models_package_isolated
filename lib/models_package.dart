756232e56ebec05da5d2f71cdca69a9114ff7b88
Amir <amirsol456@gmail.com>
2026-02-03 15:54:42 +0330

    version 0.0.2

--- lib/models_package.dart
+++ lib/models_package.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: undefined_shown_name, unused_import
 library;
 
-import 'package:shared_core/data/com/person/request.dart' as pReq;
+import 'package:shared_core/data/com/person/request.dart' as preq;
 // import 'package:shared_core/index.dart' as models;
 
 export 'package:shared_core/data/auth/user/response_data.dart' show User;
